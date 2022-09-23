e13f820a6a80c1a106b2968898a4783467535e28
yuuki-foll <tu.over.ym@gmail.com>
2022-09-23 23:50:48 +0900

    add filetable Model Class

---
+++ src/app/Models/Filedata.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Filedata extends Model
+{
+    use HasFactory;
+
+    protected $table = 'filedatas';
+    protected $fillable = 
+    [
+        'filename',
+        'text'
+    ];
+}
