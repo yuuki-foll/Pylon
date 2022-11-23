fb7e9e8618e0ff5727d8baea5e583755086741e2
yuji-matsunami <yuji.sohuto@gmail.com>
2022-11-23 14:38:17 +0900

    fix: tailwind CSSが反映されない問題を解消

--- src/vite.config.js
+++ src/vite.config.js
@@ -11,4 +11,10 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        host: true,
+        hmr: {
+            host: 'localhost',
+        },
+    }
 });
