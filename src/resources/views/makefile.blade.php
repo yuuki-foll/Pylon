9a7eb8b79636c25a275c491b4daf457b71cc9b8f
yuuki-foll <tu.over.ym@gmail.com>
2022-11-30 12:12:38 +0900

    ファイル作成画面の簡易UI設定

--- src/resources/views/makefile.blade.php
+++ src/resources/views/makefile.blade.php
@@ -1,27 +1,21 @@
-<header>
-  <h1 class='headline'>
-    <a>ファイル作成画面</a>
-  </h1>
+<x-pylon_template>
+  <x-slot name="header"></x-slot>
+  <x-slot name="slot">
+    <form method="POST" onsubmit="return false" action=" {{route('save')}}" autocomplete="off" class="mx-8 my-8">
+      @csrf
+      <div class="mb-8">
+        <label class="text-base block">ファイル名</label>
+        <input type="text" name="file_name" class="w-1/4">
+      </div>
 
-</header>
+      @if($errors->has('file_name'))
+      <label class="text-red-500">{{$errors->first('file_name')}}</label>
+      @endif
 
-<body>
-  <form method="POST" action="{{route('save')}}" autocomplete="off">
-    @csrf
-    ファイル名：<input name="file_name" type="text">
-
-    @if($errors->has('file_name'))
-      <p>{{$errors->first('file_name')}}</p>
-    @endif
-
-    <br> <br>
-
-    <input name="text" type="text">
-
-    <br> <br>
-
-    <button type="submit">
-      保存
-    </button>
-  </form>
-</body>
+      <div class="mb-8">
+        <textarea name=”text” style="width:100%; height:60vh;" class="whitespace-pre-wrap"></textarea>
+      </div>
+      <button type="button" onclick="submit();" class="bg-blue-800 hover:bg-blue-700 text-white rounded px-4 py-2">保存</button>
+    </form>
+  </x-slot>
+</x-pylon_template>
