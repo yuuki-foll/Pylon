f1f9dd9397e922554bf2bbfd9a9b493bc810da56
yuji-matsunami <yuji.sohuto@gmail.com>
2022-12-24 19:46:44 +0900

    [bug] リダイレクトがおかしいのでダッシュボードに戻すように一時的に変更

--- src/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ src/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -37,7 +37,8 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(RouteServiceProvider::HOME);
+        # return redirect()->intended(RouteServiceProvider::HOME);
+        return redirect('/dashboard');
     }
 
     /**
