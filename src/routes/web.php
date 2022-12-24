995c7fc05762a3210c70eec4ca4d88979300f667
yuji-matsunami <yuji.sohuto@gmail.com>
2022-12-24 18:45:12 +0900

    fix:react導入で動かなくなってしまった箇所を修正

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 use App\Http\Controllers\FiledataController;
 
 /*
@@ -14,12 +17,14 @@ use App\Http\Controllers\FiledataController;
 |
 */
 
-Route::get(
-    '/',
-    function () {
-        return view('welcome');
-    }
-);
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
 
 Route::get(
     '/pylon',
@@ -36,11 +41,14 @@ Route::get('/makefile', [FiledataController::class, 'edit_make_file'])
 
 Route::post('/makefile', [FiledataController::class, 'save'])->name('save');
 
-Route::get(
-    '/dashboard',
-    function () {
-        return view('dashboard');
-    }
-)->middleware(['auth'])->name('dashboard');
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-require __DIR__ . '/auth.php';
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
